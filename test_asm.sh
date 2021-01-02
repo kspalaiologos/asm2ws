55255edc51f31490acdd0dc30eda0036dc3c4617
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-01-02 22:40:31 +0100

    test tweaks, disassembly tests

--- test_asm.sh
+++ test_asm.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 data=$(cat /dev/stdin)
 ./wsi -m "./tests/$1" > "./tests/$1.ws"
+./wsi -d "./tests/$1.ws" > "./tests/$1.ws.asm"
 echo "$data" | ./wsi "./tests/$1.ws" | md5sum | grep "$2" > /dev/null
 exit $?
--- test_single.sh
+++ test_single.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-./wsi $1 "./tests/$2" | md5sum | grep "$3" > /dev/null
+./wsi $1 ./tests/$2 | md5sum | grep "$3" > /dev/null
 exit $?
