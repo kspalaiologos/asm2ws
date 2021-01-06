1fcb2a510a0bd65a8961ec190c260deddf10377d
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-01-06 10:48:29 +0100

    conflicting gitignores?

--- mktest.sh
+++ mktest.sh
@@ -1,13 +1,43 @@
 #!/bin/sh
 
-printf "$(tput setaf 5)test name:$(tput sgr0) "
+_prompt() {
+    echo "$(tput setaf 7)$1:$(tput sgr0) "
+    cat > "$name.$2"
+}
+
+printf "$(tput setaf 7)test name:$(tput sgr0) "
 read name
 
-echo "program: "
-cat > "tests/ws-build-run/$name.asm"
+read -n 1 -p "type: (r)ebuild/(b)uild" ans;
 
-echo "input: "
-cat > "tests/ws-build-run/$name.in"
+cd tests
 
-echo "output: "
-cat > "tests/ws-build-run/$name.out"
+case $ans in
+    r|R)
+        cd "ws-rebuild"
+
+        _prompt "program" "bak"
+        _prompt "input" "ws.in"
+        _prompt "output" "ws.out"
+
+        cp "$name.ws.in" "$name.ws.asm.ws.in"
+        cp "$name.ws.out" "$name.ws.asm.ws.out"
+
+        [ $(wc -c < "$name.ws.in") -eq 0 ] \
+            && rm "$name.ws.in" \
+            && "$name.ws.asm.ws.in"
+
+        ./wsi -m "$name.bak" > "$name.ws"
+        ;;
+    b|B)
+        cd "ws-build-run"
+
+        _prompt "program" "asm"
+        _prompt "input" "ws.in"
+        _prompt "output" "ws.out"
+
+        [ $(wc -c < "$name.in") -eq 0 ] && rm "$name.in"
+        ;;
+    *)
+        exit;;
+esac
